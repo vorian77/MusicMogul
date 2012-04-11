a99c11f8bb38d60c134c9384937628992f67e9e6
Phyllip Hall <phyl@fanhelp.us>
2012-04-11 16:17:35 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mvp2::Application.config.secret_token = 'e5ba838816508da1ffcaca5fb4914acf5d1315d9c77775f4bc1889e1037cdfb803094168249be837574979b95adb0e5380c7aa6451df95d53bf0e17137a31352'
