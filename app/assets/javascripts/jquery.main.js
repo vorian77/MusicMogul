b5c416314e165ce3e1fac90a93ce96f11ef081cd
Phyllip Hall <phyl@fanhelp.us>
2012-04-17 11:26:21 -0400

    Removed clear form fields code from js

--- app/assets/javascripts/jquery.main.js
+++ app/assets/javascripts/jquery.main.js
@@ -1,18 +1,7 @@
 jQuery(function(){
-	initClear();
 	initCarusel();
 });
 
-// initClear
-function initClear(){
-	clearFormFields({
-		clearInputs: true,
-		clearTextareas: true,
-		passwordFieldText: true,
-		addClassFocus: "focus",
-		filterClass: "default"
-	});
-}
 //initCarusel
 function initCarusel(){
 	jQuery('div.carousel').scrollGallery({
@@ -261,78 +250,6 @@ jQuery.fn.scrollGallery = function(_options){
 		}
 		if(_afterInit && typeof _afterInit === 'function') _afterInit(_this, _slides);
 	});
-}
-// clearFormFields
-function clearFormFields(o){
-	if (o.clearInputs == null) o.clearInputs = true;
-	if (o.clearTextareas == null) o.clearTextareas = true;
-	if (o.passwordFieldText == null) o.passwordFieldText = false;
-	if (o.addClassFocus == null) o.addClassFocus = false;
-	if (!o.filter) o.filter = "default";
-	if(o.clearInputs) {
-		var inputs = document.getElementsByTagName("input");
-		for (var i = 0; i < inputs.length; i++ ) {
-			if((inputs[i].type == "text" || inputs[i].type == "password") && inputs[i].className.indexOf(o.filterClass)) {
-				inputs[i].valueHtml = inputs[i].value;
-				inputs[i].onfocus = function ()	{
-					if(this.valueHtml == this.value) this.value = "";
-					if(this.fake) {
-						inputsSwap(this, this.previousSibling);
-						this.previousSibling.focus();
-					}
-					if(o.addClassFocus && !this.fake) {
-						this.className += " " + o.addClassFocus;
-						this.parentNode.className += " parent-" + o.addClassFocus;
-					}
-				}
-				inputs[i].onblur = function () {
-					if(this.value == "") {
-						this.value = this.valueHtml;
-						if(o.passwordFieldText && this.type == "password") inputsSwap(this, this.nextSibling);
-					}
-					if(o.addClassFocus) {
-						this.className = this.className.replace(o.addClassFocus, "");
-						this.parentNode.className = this.parentNode.className.replace("parent-"+o.addClassFocus, "");
-					}
-				}
-				if(o.passwordFieldText && inputs[i].type == "password") {
-					var fakeInput = document.createElement("input");
-					fakeInput.type = "text";
-					fakeInput.value = inputs[i].value;
-					fakeInput.className = inputs[i].className;
-					fakeInput.fake = true;
-					inputs[i].parentNode.insertBefore(fakeInput, inputs[i].nextSibling);
-					inputsSwap(inputs[i], null);
-				}
-			}
-		}
-	}
-	if(o.clearTextareas) {
-		var textareas = document.getElementsByTagName("textarea");
-		for(var i=0; i<textareas.length; i++) {
-			if(textareas[i].className.indexOf(o.filterClass)) {
-				textareas[i].valueHtml = textareas[i].value;
-				textareas[i].onfocus = function() {
-					if(this.value == this.valueHtml) this.value = "";
-					if(o.addClassFocus) {
-						this.className += " " + o.addClassFocus;
-						this.parentNode.className += " parent-" + o.addClassFocus;
-					}
-				}
-				textareas[i].onblur = function() {
-					if(this.value == "") this.value = this.valueHtml;
-					if(o.addClassFocus) {
-						this.className = this.className.replace(o.addClassFocus, "");
-						this.parentNode.className = this.parentNode.className.replace("parent-"+o.addClassFocus, "");
-					}
-				}
-			}
-		}
-	}
-	function inputsSwap(el, el2) {
-		if(el) el.style.display = "none";
-		if(el2) el2.style.display = "inline";
-	}
 }
 
 $(document).ready(function() {
