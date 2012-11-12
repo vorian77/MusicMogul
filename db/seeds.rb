13a5196ff45fdc5be0b04786289bcaaa7c66a6f5
Michael Haddad <michael@ludditetechnology.com>
2012-11-12 13:45:13 -0500

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-FactoryGirl.create_list(:entry, 3)
+Entry.create(community_name: "The Doors",
+             hometown: "Los Angeles",
+             genre: "Rock",
+             bio: %Q{The Doors were an American rock band formed in 1965 in Los Angeles, California, with vocalist Jim Morrison, keyboardist Ray Manzarek, drummer John Densmore and guitarist Robby Krieger. The band took its name from Aldous Huxley's book The Doors of Perception, the title of which was a reference to a William Blake quotation: "If the doors of perception were cleansed every thing would appear to man as it is, infinite."})
+
+Entry.create(community_name: "Eminem",
+             hometown: "Detroit",
+             genre: "Hip Hop",
+             bio: %Q{Marshall Bruce Mathers III (born October 17, 1972), better known by his stage name Eminem and by his alter ego Slim Shady, is an American rapper, record producer, songwriter and actor.})
+
+Entry.create(community_name: "Aretha Franklin",
+             hometown: "Detroit",
+             genre: "R&B",
+             bio: %Q{Aretha Louise Franklin (born March 25, 1942) is an American musician, singer, songwriter, and pianist. In a recording career that has spanned over half a century, Franklin's repertoire has included gospel, jazz, blues, R&B, pop, rock and funk.})
