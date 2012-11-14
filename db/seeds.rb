eabe0c1f299e3d1e1258d954c83a25b60977303c
Michael Haddad <michael@ludditetechnology.com>
2012-11-13 22:37:10 -0500

    Add Phyl to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 User.create(email: "test@fanhelp.us",
+            password: "password",
+            confirmed_at: Date.today)
+
+User.create(email: "phyl@fanhelp.us",
             password: "password",
             confirmed_at: Date.today)
 
