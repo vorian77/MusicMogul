3e7bbed2fe2bf6928e6cd1ee25f5a51a2e848466
Michael Haddad <michael@ludditetechnology.com>
2012-11-19 11:48:15 -0500

    Entries with explicit content are filtered from auditions based on user preference

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @entries = if user_signed_in?
-                 Entry.unevaluated_by(current_user).order("random()")
-               else
-                 Entry.order("random()")
-               end
+    if user_signed_in?
+      @entries = Entry.unevaluated_by(current_user).order("random()")
+      @entries = @entries.where(has_explicit_content: false) unless current_user.show_explicit_videos?
+    else
+      @entries = Entry.order("random()")
+    end
   end
 
   def video
