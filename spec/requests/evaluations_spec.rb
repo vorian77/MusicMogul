0a9237dda9a06dfd0dd6da0e7005fb4f8e3ffb39
Michael Haddad <michael@ludditetechnology.com>
2012-12-04 13:34:48 -0500

    Fix evaluations spec again with ellipsis

--- spec/requests/evaluations_spec.rb
+++ spec/requests/evaluations_spec.rb
@@ -28,7 +28,7 @@ feature "evaluations" do
 
     within("div.bottom-list > ul") do
       page.should have_content user.name
-      page.should have_content comment.first(178)
+      page.should have_content comment.first(150)
     end
   end
 
