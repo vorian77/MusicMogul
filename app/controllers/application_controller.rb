61133367a830e81db7cc3893f31783712436429d
Michael Haddad <michael@ludditetechnology.com>
2012-12-06 14:19:13 -0500

    Add simple 404/500 pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
 
   layout :layout_by_resource
 
+  unless Rails.configuration.consider_all_requests_local
+    rescue_from Exception, with: :render_error
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+    rescue_from ActionController::UnknownController, with: :render_not_found
+    rescue_from ActionController::UnknownAction, with: :render_not_found
+  end
+
   def authenticate_admin!
     authenticate_user!
     redirect_to new_user_session_path unless current_user.admin?
@@ -16,5 +23,23 @@ class ApplicationController < ActionController::Base
     else
       "application"
     end
+  end
+
+  private
+
+  def render_not_found(e)
+    respond_to do |format|
+      format.html { render template: "errors/404", status: 404 }
+      format.all { render nothing: true, status: 404 }
+    end
+    true
+  end
+
+  def render_error(e)
+    respond_to do |format|
+      format.html { render template: "errors/500", status: 500 }
+      format.all { render nothing: true, status: 500 }
+    end
+    true
   end
 end
