e4914cb19b969867f7a342f656bca5f5c3a554b9
Michael Haddad <michael@ludditetechnology.com>
2012-12-06 22:19:19 -0500

    Scope links in pages spec

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,17 +1,17 @@
 require "spec_helper"
 
 feature "pages" do
-  scenario "user visits about us", js: true do
+  scenario "user visits about us", js: true, driver: :selenium do
     visit root_path
-    click_link "About Us"
-    current_path.should == notices_path
+    within("div#footer") { click_link "About Us" }
     page.should have_content "Our mission is to help unheralded singers and bands to advance their career in professional music."
+    current_path.should == notices_path
   end
 
-  scenario "user visits contact us", js: true do
+  scenario "user visits contact us", js: true, driver: :selenium do
     visit root_path
-    click_link "Contact Us"
-    current_path.should == notices_path
+    within("div#footer") { click_link "Contact Us" }
     page.should have_content "Have a question? Drop us a line!"
+    current_path.should == notices_path
   end
 end
