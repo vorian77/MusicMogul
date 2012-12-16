18f94fba533b247d145c69bace7f6d31e54bf3f0
Michael Haddad <michael@ludditetechnology.com>
2012-12-16 16:22:39 -0500

    Add staging environment

---
+++ config/environments/staging.rb
@@ -0,0 +1,13 @@
+Mvp2::Application.configure do
+  config.cache_classes = true
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+  config.serve_static_assets = false
+  config.assets.compress = true
+  config.assets.compile = false
+  config.assets.digest = true
+  config.assets.precompile += %w[active_admin.css active_admin.js]
+  config.i18n.fallbacks = true
+  config.active_support.deprecation = :notify
+  config.action_mailer.default_url_options = { :host => 'music-mogul-staging.herokuapp.com' }
+end
