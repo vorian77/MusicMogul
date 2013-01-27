0eff0efb7ba55bea4daaefb6c412b4db7ecd2a41
Michael Haddad <michael@ludditetechnology.com>
2013-01-27 17:37:26 -0500

    Fix follows spec

--- spec/requests/follows_spec.rb
+++ spec/requests/follows_spec.rb
@@ -1,51 +1,32 @@
 require "spec_helper"
 
 feature "follows" do
-  scenario "user follows and unfollows from the list view", js: true, driver: :selenium do
+  scenario "user follows and unfollows from My Evaluations", js: true, driver: :selenium do
     user = users(:confirmed_user)
     login_as user, scope: :user
 
-    visit leaderboard_path
+    visit root_path
+    click_link "My Evaluations"
+    current_path.should == evaluations_path
 
-    entry = user.evaluations.first.entry
-    within("div#entry_#{entry.id}") do
+    evaluation = user.evaluations.first
+    within("div#evaluation_#{evaluation.id}") do
       click_link "Follow"
       page.should have_css "a", text: "Following"
     end
 
-    visit leaderboard_path
+    visit evaluations_path
 
-    within("div#entry_#{entry.id}") do
+    within("div#evaluation_#{evaluation.id}") do
       page.should have_css "a", text: "Following"
       click_link "Following"
       page.should have_css "a", text: "Follow"
     end
 
-    visit leaderboard_path
+    visit evaluations_path
 
-    within("div#entry_#{entry.id}") do
+    within("div#evaluation_#{evaluation.id}") do
       page.should have_css "a", text: "Follow"
     end
-  end
-
-  scenario "user views follows" do
-    user = users(:confirmed_user)
-    login_as user, scope: :user
-
-    visit root_path
-    pending "contest start flow"
-
-    click_link "Following"
-    current_path.should == follows_path
-
-    Entry.find_each do |entry|
-      page.should have_no_content entry.stage_name
-    end
-
-    entry = Entry.first
-    FactoryGirl.create(:follow, user: user, entry: entry)
-
-    visit follows_path
-    page.should have_content entry.stage_name
   end
 end
