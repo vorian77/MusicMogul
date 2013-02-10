af22b74b967c28c261393f3dee7f91a6cf742e6d
Michael Haddad <michael@ludditetechnology.com>
2013-02-10 14:25:52 -0500

    Add truste user to basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,7 @@ class ApplicationController < ActionController::Base
 
   def perform_basic_authentication
     authenticate_or_request_with_http_basic do |username, password|
-      username == "mogul" && password == "detroit1"
+      (username == "mogul" || username == "truste") && password == "detroit1"
     end
   end
 
