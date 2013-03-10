e1a4988df35712a205188f70b1d09e4a046bf61a
Michael Haddad <michael@ludditetechnology.com>
2013-03-10 10:37:11 -0400

    Tweaks to landing

--- app/controllers/prospects_controller.rb
+++ app/controllers/prospects_controller.rb
@@ -2,6 +2,6 @@ class ProspectsController < ApplicationController
   def create
     @prospect = Prospect.new(email: params[:email])
     @prospect.save
-    render text: "Awesome to have you on board! We'll let you know when we launch, which will happen really soon!"
+    render text: "Awesome to have you on board! We'll let you know when we launch. See you soon!"
   end
 end
