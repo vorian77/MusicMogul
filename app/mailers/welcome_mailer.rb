2c20a3a013300a38716827be54f902e406316f0f
Michael Haddad <michael@ludditetechnology.com>
2013-04-01 14:10:50 -0400

    New artists receive welcome email

---
+++ app/mailers/welcome_mailer.rb
@@ -0,0 +1,9 @@
+class WelcomeMailer < ActionMailer::Base
+  default from: "musicmogul-noreply@musicmogul.com"
+
+  def new_artist(user)
+    @user = user
+    mail(to: user.email,
+         subject: "Welcome to Music Mogul!")
+  end
+end
---
+++ spec/mailers/welcome_mailer_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe WelcomeMailer do
+  describe "#new_artist" do
+    let(:user) { Entry.first.user }
+    let(:email) { WelcomeMailer.new_artist(user).deliver }
+
+    subject { email }
+
+    its(:to) { should == [user.email] }
+    its(:from) { should == ["musicmogul-noreply@musicmogul.com"] }
+    its(:subject) { should == "Welcome to Music Mogul!" }
+    its(:body) { should include(%{Hi #{user.username}}) }
+  end
+end
