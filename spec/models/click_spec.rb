82d2aa2136d0af84bde1ab494e6c55582ef3ea57
Michael Haddad <michael@ludditetechnology.com>
2013-04-05 10:53:25 -0400

    Add click spec

--- spec/models/click_spec.rb
+++ spec/models/click_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Click do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { should belong_to :user }
+    it { should belong_to :entry }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:user).with_message("User is required") }
+    it { should validate_presence_of(:entry).with_message("Entry is required") }
+    it { should ensure_inclusion_of(:object).in_array(Click::OBJECTS) }
+  end
 end
