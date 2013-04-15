d7440ffcb678ed7f3319190b0abb8eb39f159e53
Michael Haddad <michael@ludditetechnology.com>
2013-04-15 19:19:14 -0400

    Add inviter to admin users csv

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -15,4 +15,39 @@ ActiveAdmin.register User do
     column :created_at
     column :updated_at
   end
+
+  csv do
+    column :id
+    column :username
+    column :email
+    column :inviter do |user|
+      user.inviter.username if user.inviter.present?
+    end
+    column :created_at
+    column :updated_at
+    column :encrypted_password
+    column :reset_password_token
+    column :reset_password_sent_at
+    column :remember_created_at
+    column :sign_in_count
+    column :current_sign_in_at
+    column :last_sign_in_at
+    column :current_sign_in_ip
+    column :last_sign_in_ip
+    column :confirmation_token
+    column :confirmed_at
+    column :confirmation_sent_at
+    column :unconfirmed_email
+    column :hometown
+    column :admin
+    column :show_explicit_videos
+    column :receive_email_updates
+    column :profile_photo
+    column :referral_token
+    column :shortened_referral_link
+    column :musician
+    column :tos
+    column :time_zone
+    column :cached_points
+  end
 end
