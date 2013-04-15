3ded0ca06c82021c06923bda233a11e5dbda78f7
Michael Haddad <michael@ludditetechnology.com>
2013-04-15 19:14:55 -0400

    Add inviter to admin users table

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,18 @@
 ActiveAdmin.register User do
-  
+  index do
+    column :id
+    column :username
+    column :email
+    column "Inviter" do |user|
+      link_to user.inviter.username, user.inviter if user.inviter.present?
+    end
+    column :confirmed_at
+    column :profile_photo
+    column :referral_token
+    column :shortened_referral_link
+    column :musician
+    column :cached_points
+    column :created_at
+    column :updated_at
+  end
 end
