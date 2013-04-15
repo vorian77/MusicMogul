81ad3fe0db0efa5dcab604f2a5c25d42c5bd180b
Michael Haddad <michael@ludditetechnology.com>
2013-04-15 11:34:47 -0400

    Add fan email link to friends page

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -3,5 +3,6 @@ class FriendsController < ApplicationController
 
   def index
     @invited_users = current_user.invited_users.includes(:entries).order("created_at")
+    @entry = current_user.entries.first if current_user.entries.present?
   end
 end
