d758570c3929dffac14a2be336a3bd118e6629ed
Michael Haddad <michael@ludditetechnology.com>
2013-04-16 14:38:18 -0400

    Update admin evals

--- app/admin/evaluations.rb
+++ app/admin/evaluations.rb
@@ -1,3 +1,35 @@
 ActiveAdmin.register Evaluation do
-  
+  index do
+    column :id
+    column :user_id
+    column "Username" do |evaluation|
+      evaluation.user.username
+    end
+    column :created_at
+    column :updated_at
+    column :entry_id
+    column :comment
+    column :music_score
+    column :presentation_score
+    column :vocals_score
+    column :overall_score
+    column :share_email
+  end
+
+  csv do
+    column :id
+    column :user_id
+    column "Username" do |evaluation|
+      evaluation.user.username
+    end
+    column :created_at
+    column :updated_at
+    column :entry_id
+    column :comment
+    column :music_score
+    column :presentation_score
+    column :vocals_score
+    column :overall_score
+    column :share_email
+  end
 end
