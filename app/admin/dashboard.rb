c0013e1f2f46f2717a9c9cc6eb48db89a1e66cb6
Michael Haddad <michael@ludditetechnology.com>
2013-04-27 14:39:24 -0400

    Add contestant data to dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,33 +1,23 @@
 ActiveAdmin.register_page "Dashboard" do
 
-  menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
+  menu :priority => 1, :label => proc { I18n.t("active_admin.dashboard") }
 
-  content :title => proc{ I18n.t("active_admin.dashboard") } do
-    div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span "Welcome to Active Admin. This is the default dashboard page."
-        small "To add dashboard sections, checkout 'app/admin/dashboards.rb'"
+  content :title => proc { I18n.t("active_admin.dashboard") } do
+    columns do
+      column do
+        panel "Contestant Data", id: "contestant_data" do
+          render "admin/contestant_data"
+        end
       end
     end
+  end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  controller do
+    def index
+      if params[:entry].try(:[], :id).present?
+        @entry = Entry.find(params[:entry][:id])
+        @user = @entry.user
+      end
+    end
+  end
 end
